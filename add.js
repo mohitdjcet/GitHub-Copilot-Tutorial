f5ca8fac9b32d0fc5fa91500a922fa9ee85388db
Mohit Kumar <mohitkumar@Mohits-MacBook-Air.local>
2025-03-24 16:57:25 +0530

    Update final

---
+++ add.js
@@ -0,0 +1,15 @@
+// add function
+// function add(a, b) {
+//   return a + b;
+// }
+
+// add a discription and perameter for the above funtion
+/**
+ * Add two numbers
+ * @param {number} a The first number
+ * @param {number} b The second number
+ * @returns {number} The sum of the two numbers
+ */
+function add(a, b) {
+  return a + b;
+}
